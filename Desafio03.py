193d2cb514c86dae8a9cf6cd41ccbb83d63c95c5
Igor Rubio <ighor_souza@hotmail.com>
2022-12-07 22:20:42 -0300

    Criacao do desafio03

--- Desafio03.py
+++ Desafio03.py
@@ -1,6 +1,11 @@
-
-
 numero1 = int (input ('Digite o primeiro numero '))
 numero2 = int (input ('digite o segundo numero '))
 soma = numero1 +  numero2
-print(soma)
+print('O total é ', soma)
+
+
+
+
+
+
+

cddc8e6dea6a33e0c9a3cf8529f055d089d5e721
Igor Rubio <ighor_souza@hotmail.com>
2022-12-09 22:37:09 -0300

    Criacao do desafio 004

---
+++ aula06a.py
@@ -0,0 +1,5 @@
+numero1 = int (input ('Digite o primeiro numero '))
+numero2 = int (input ('digite o segundo numero '))
+soma = numero1 +  numero2
+print('A soma entre {0} e {1} é {2}'.format(numero1, numero2, soma))
+
---
+++ desafio004.py
@@ -0,0 +1,4 @@
+# Este exercicio verifica o tipo de dados digitado e se é numerico
+entrada = input('Digite alguma coisa ')
+print(type(entrada))
+print(entrada.isnumeric())

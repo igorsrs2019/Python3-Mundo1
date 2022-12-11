e404bce8c23b7a458b170beed7ffdf1cdc6ca5c4
Igor Rubio <ighor_souza@hotmail.com>
2022-12-10 22:30:09 -0300

    Alteracao do exercicio

--- desafio004.py
+++ desafio004.py
@@ -1,4 +1,10 @@
-# Este exercicio verifica o tipo de dados digitado e se é numerico
+# Este exercicio verifica o tipo de dados digitado e identifica as caracteristicas dele.
 entrada = input('Digite alguma coisa ')
 print(type(entrada))
-print(entrada.isnumeric())
+print('Este valor {} é numerico?', entrada.isnumeric())
+print('Todas as letra que foram digitadas sao maiuscula?', entrada.isupper())
+print('Todos os caracteres digitados sao letras?', entrada.isalpha())
+print('O valor digitado foi um espaco?', entrada.isspace())
+print('Todos os caracteres estao em minusculo?',entrada.islower())
+print('Esta capitalizada?', entrada.istitle())
+ 

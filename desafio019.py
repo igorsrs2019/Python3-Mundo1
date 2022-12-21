d5870b24c047ceb20191fa63d3d3dc6cdf76d8bf
Igor Rubio <ighor_souza@hotmail.com>
2022-12-20 21:40:35 -0300

    Alteracao dos desafios 019 e 021

--- desafio019.py
+++ desafio019.py
@@ -1,9 +1,10 @@
-# O Programa solicita 4 nomes e exibe um
+# O Programa solicita 4 nomes e sorteia um nome
 from random import choice
 
-n1 = input('Digite o primeiro nome: ')
-n2 = input ('Digite o segundo nome: ')
-n3 =  input ('Digite o terceiro nome: ')
-n4 = input ('Digite o quarto nome: ')
-escolhido = choice ( (n1, n2, n3, n4))
+n1 = str(input('Digite o primeiro nome: '))
+n2 = str(input ('Digite o segundo nome: '))
+n3 =  str(input ('Digite o terceiro nome: '))
+n4 = str(input ('Digite o quarto nome: '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
 print ('Entre os 4 nomes digitados, o escolhido foi {}'.format(escolhido))
--- desafio021.py
+++ desafio021.py
@@ -1,4 +1,5 @@
 #Este programa reproduz uma musica
+#Para a musica tocar ẽ necessario que o arquivo .mp3 esteja no diretorio
 from webbrowser import open
 
 open("Cancao.mp3")

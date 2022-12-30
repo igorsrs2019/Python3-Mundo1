32408515542e1bf8efb0c9436597187f695c2bc8
Igor Rubio <ighor_souza@hotmail.com>
2022-12-30 11:20:17 -0300

    Resolucao do desafio 0027

---
+++ desafio027.py
@@ -0,0 +1,8 @@
+# este programa solicita seu nome completo e exibe ele com todas as letras maiusculas, todas em minusculas, conta quantos letras ele tem ao total e informa quantas letras tem o primeiro nome.
+
+nome = str (input('Digite seu nome completo: '))
+print ('Seu nome em maisculas é {} '.format(nome.upper()))   
+print ('Seu nome em minusculas é {} '.format(nome.lower()))
+print ('Seu nome tem ao todo {}'.format(len(nome.strip()))+ ' letras')
+lista = (nome.split())
+print ('Seu primeiro nome é {} e tem {}'.format(lista[0], len(lista[0]))+ ' letras')

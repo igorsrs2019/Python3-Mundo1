0d3769965ef6ed45d7b92cbed11b53be7b033664
Igor Rubio <ighor_souza@hotmail.com>
2022-12-30 12:00:49 -0300

    Correcao do desafio 022

--- desafio022.py
+++ desafio022.py
@@ -1,8 +1,7 @@
 #Este programa recebe um nome completo e exibe. O nome com todas as letras maiusculas, todas minusculas, mostra quantas letras tem todos os caracteres sem contar o espaco e quantas letras tem no primeiro nome.
 
-nome = str (input ('Digite o primeiro nome: '))
-print (nome.upper())
-print(nome.lower())
-print (len(nome.strip()))
-lista = (nome.split())
-print (len(lista[0]))
+nome = str (input ('Digite o primeiro nome: ')).strip()
+print ('Seu nome em maisculas é {} '.format(nome.upper()))
+print('Seu nome em minusculas é {} '.format(nome.lower()))
+print ('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+print ('Seu primeiro nome tem {} letras'.format(nome.find(' ')))

af178bfe1642fe2e23f57f7dbbeca96be6798059
Igor Rubio <ighor_souza@hotmail.com>
2023-01-02 21:19:04 -0300

    Criacao do desafio 029

--- desafio029.py
+++ desafio029.py
@@ -1,8 +1,9 @@
 #Este programa solicita que o condutor insira a velocidade, e se caso a mesma passar de 80Km o condutor é multado em R$7,00 por KM.
 
 v = int (input('Digite a velocidade atual: '))
-m = v * 7
-if v >= 80:
+km = v - 80
+m = km * 7 
+if v > 80:
     print('A sua velocidade é de {}Km e voce foi multado em R${}'.format(v, m))
 else:
     print('Esta dentro do limite de velocidade')

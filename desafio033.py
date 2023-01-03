584affc61486303aea89b0bc105bf7ead07b16d8
Igor Rubio <ighor_souza@hotmail.com>
2023-01-03 17:35:24 -0300

    Criacao dos desafios 033 e 024

---
+++ desafio033.py
@@ -0,0 +1,4 @@
+# Este programa recebe 3 numeros e informa qual é o maior e qual é o menor
+n1 = int (input('Digite o primeiro numero: '))
+n2 = int (input('Digite o segundo numero: '))
+n3 = int (input('Digite o terceiro numero: '))
--- desafio034.py
+++ desafio034.py
@@ -1,4 +1,5 @@
-s = float (input('Digite o seu Sálario R$: '))
+#Este programa recebe o salário e adiciona um aumento de 10% se o salario for igual ou acima de R$ 1250,00, e se for abaixo recebe o aumento de 15%
+s = float (input('Digite o seu salário R$: '))
 
 if s >= 1250.00:
     print ('Seu salário é R${} seu aumento é de 10%, ao total vai ficar R${}'.format(s, ((s * 10) /100) + s))

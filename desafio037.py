6a520bfce8b3880c23e8d4a043c223152c227b2c
Igor Rubio <ighor_souza@hotmail.com>
2023-10-31 17:30:58 -0300

    Criaçao do desafio 0037

---
+++ desafio037.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um número inteiro: '))
+print ('''Éscolha uma das bases para conversão:
+ [1] converter para BINÁRIO
+ [2] converter para OCTAL
+ [3] convertet para HEXADECIMAL''')      
+
+opcao = int(input('Sua opção: '))
+
+if opcao
+       
+       
+    
